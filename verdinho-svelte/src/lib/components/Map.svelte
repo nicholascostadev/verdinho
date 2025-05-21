0a4163891e66535667e1d8a848238c1718f3c045
Nicholas Costa <nicholascostadev@gmail.com>
2025-05-20 21:34:18 -0300

    refactor: batch process map quadrants to improve performance and maintainability

--- verdinho-svelte/src/lib/components/Map.svelte
+++ verdinho-svelte/src/lib/components/Map.svelte
@@ -214,96 +214,100 @@
 	$effect(() => {
 		if (!map) return;
 
-		if (!isProcessingQuadrant && mapState.processingQuadrants.length > 0 && map && L) {
+		const BATCH_SIZE = 2;
+		if (!isProcessingQuadrant && mapState.processingQuadrants.length > 0 && L) {
 			isProcessingQuadrant = true;
-			const quadrant = mapState.processingQuadrants[0];
-			const processQuadrant = async () => {
-				try {
-					const recommendedPlantForArea = await getRecommendedPlant({
-						latMin: quadrant.latMin,
-						lonMin: quadrant.lonMin,
-						latMax: quadrant.latMax,
-						lonMax: quadrant.lonMax
-					});
+			const batch = mapState.processingQuadrants.slice(0, BATCH_SIZE);
+			const processBatch = async () => {
+				await Promise.all(
+					batch.map(async (quadrant) => {
+						try {
+							const recommendedPlantForArea = await getRecommendedPlant({
+								latMin: quadrant.latMin,
+								lonMin: quadrant.lonMin,
+								latMax: quadrant.latMax,
+								lonMax: quadrant.lonMax
+							});
 
-					if (recommendedPlantForArea) {
-						const quadrantData: QuadrantData = {
-							lat: quadrant.latMin,
-							lon: quadrant.lonMin,
-							quadrantBounds: quadrant,
-							plantRecommendation: recommendedPlantForArea.prediction,
-							analyzedAreaData: recommendedPlantForArea.features_used
-						};
-						mapState.addQuadrant(quadrantData);
-
-						const popup = popupClasses();
-						const bodyWrapper = document.createElement('div');
-						bodyWrapper.classList.add(...popup.body.split(' '));
-
-						for (const key in recommendedPlantForArea.prediction) {
-							const rowWrapper = document.createElement('div');
-							rowWrapper.classList.add(...popup.row.split(' '));
-							const icon = document.createElement('span');
-							icon.classList.add(...popup.icon.split(' '));
-							const label = document.createElement('strong');
-							label.classList.add(...popup.label.split(' '));
-							const value = document.createElement('span');
-							value.classList.add(...popup.value.split(' '));
-							value.textContent = `${key}: ${recommendedPlantForArea.prediction[key as keyof typeof recommendedPlantForArea.prediction]}`;
-							rowWrapper.appendChild(icon);
-							rowWrapper.appendChild(label);
-							rowWrapper.appendChild(value);
-							bodyWrapper.appendChild(rowWrapper);
-						}
-						for (const key in recommendedPlantForArea.features_used) {
-							const rowWrapper = document.createElement('div');
-							rowWrapper.classList.add(...popup.row.split(' '));
-							const icon = document.createElement('span');
-							icon.classList.add(...popup.icon.split(' '));
-							const label = document.createElement('strong');
-							label.classList.add(...popup.label.split(' '));
-							const value = document.createElement('span');
-							value.classList.add(...popup.value.split(' '));
-							value.textContent = `${key}: ${recommendedPlantForArea.features_used[key as keyof typeof recommendedPlantForArea.features_used]}`;
-							rowWrapper.appendChild(icon);
-							rowWrapper.appendChild(label);
-							rowWrapper.appendChild(value);
-							bodyWrapper.appendChild(rowWrapper);
-						}
-
-						L.rectangle(
-							[
-								[quadrant.latMin, quadrant.lonMin],
-								[quadrant.latMax, quadrant.lonMax]
-							],
-							{
-								color: 'oklch(79.2% 0.209 151.711)',
-								weight: 1,
-								fillOpacity: 0.6
-							}
-						)
-							.on('click', () => {
+							if (recommendedPlantForArea) {
 								const quadrantData: QuadrantData = {
 									lat: quadrant.latMin,
 									lon: quadrant.lonMin,
 									quadrantBounds: quadrant,
-									analyzedAreaData: recommendedPlantForArea.features_used,
-									plantRecommendation: recommendedPlantForArea.prediction
+									plantRecommendation: recommendedPlantForArea.prediction,
+									analyzedAreaData: recommendedPlantForArea.features_used
 								};
-								quadrantModalStore.updateQuadrant(quadrantData);
-								quadrantModalStore.openModal();
-							})
-							.addTo(map);
-					}
-				} catch (e) {
-					console.error('Error processing quadrant:', e);
-					mapState.removeProcessingQuadrant(quadrant);
-				} finally {
-					mapState.removeProcessingQuadrant(quadrant);
-					isProcessingQuadrant = false;
-				}
+								mapState.addQuadrant(quadrantData);
+
+								const popup = popupClasses();
+								const bodyWrapper = document.createElement('div');
+								bodyWrapper.classList.add(...popup.body.split(' '));
+
+								for (const key in recommendedPlantForArea.prediction) {
+									const rowWrapper = document.createElement('div');
+									rowWrapper.classList.add(...popup.row.split(' '));
+									const icon = document.createElement('span');
+									icon.classList.add(...popup.icon.split(' '));
+									const label = document.createElement('strong');
+									label.classList.add(...popup.label.split(' '));
+									const value = document.createElement('span');
+									value.classList.add(...popup.value.split(' '));
+									value.textContent = `${key}: ${recommendedPlantForArea.prediction[key as keyof typeof recommendedPlantForArea.prediction]}`;
+									rowWrapper.appendChild(icon);
+									rowWrapper.appendChild(label);
+									rowWrapper.appendChild(value);
+									bodyWrapper.appendChild(rowWrapper);
+								}
+								for (const key in recommendedPlantForArea.features_used) {
+									const rowWrapper = document.createElement('div');
+									rowWrapper.classList.add(...popup.row.split(' '));
+									const icon = document.createElement('span');
+									icon.classList.add(...popup.icon.split(' '));
+									const label = document.createElement('strong');
+									label.classList.add(...popup.label.split(' '));
+									const value = document.createElement('span');
+									value.classList.add(...popup.value.split(' '));
+									value.textContent = `${key}: ${recommendedPlantForArea.features_used[key as keyof typeof recommendedPlantForArea.features_used]}`;
+									rowWrapper.appendChild(icon);
+									rowWrapper.appendChild(label);
+									rowWrapper.appendChild(value);
+									bodyWrapper.appendChild(rowWrapper);
+								}
+
+								L.rectangle(
+									[
+										[quadrant.latMin, quadrant.lonMin],
+										[quadrant.latMax, quadrant.lonMax]
+									],
+									{
+										color: 'oklch(79.2% 0.209 151.711)',
+										weight: 1,
+										fillOpacity: 0.6
+									}
+								)
+									.on('click', () => {
+										const quadrantData: QuadrantData = {
+											lat: quadrant.latMin,
+											lon: quadrant.lonMin,
+											quadrantBounds: quadrant,
+											analyzedAreaData: recommendedPlantForArea.features_used,
+											plantRecommendation: recommendedPlantForArea.prediction
+										};
+										quadrantModalStore.updateQuadrant(quadrantData);
+										quadrantModalStore.openModal();
+									})
+									.addTo(map);
+							}
+						} catch (e) {
+							console.error('Error processing quadrant:', e);
+						} finally {
+							mapState.removeProcessingQuadrant(quadrant);
+						}
+					})
+				);
+				isProcessingQuadrant = false;
 			};
-			processQuadrant();
+			processBatch();
 		}
 	});
 
